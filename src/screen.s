b2539021d64b2a16ec39b72834ee11ca6a301a64
m1urah <173448736+m1urah@users.noreply.github.com>
2026-05-02 14:55:03 +0200

    Add panel data structures and update screen drawing logic

--- src/screen.s
+++ src/screen.s
@@ -1,5 +1,5 @@
 global setup_board
-extern write_to_screen
+extern write_to_screen, strlen
 default rel
 
 section .rodata
@@ -11,6 +11,69 @@ section .rodata
     hello_world_len     equ $-hello_world
     hello_world_2       db "Hello, World 2!"
     hello_world_3       db "Hello, World 3!"
+
+    ;# === PANELS ===
+    ; Cursor positioning ANSI escape codes starts with (1,1) not (0,0) 
+    panel_stats:
+        db 5    ; height
+        db 1    ; col (X)
+        db 1    ; line(Y)
+        ; utf-8 chars are multi-byte, null terminator marks end of row
+        db "┏━━━━━━━Stats━━━━━━━┓", 0
+        db "┃ Score       10420 ┃", 0
+        db "┃ Level           5 ┃", 0
+        db "┃ Lines          42 ┃", 0
+        db "┗━━━━━━━━━━━━━━━━━━━┛", 0
+
+    panel_next:
+        db 5
+        db 56
+        db 1
+        db "┏━━━━━━━Next━━━━━━━┓", 0
+        db "┃                  ┃", 0
+        db "┃     ████████     ┃", 0
+        db "┃                  ┃", 0
+        db "┗━━━━━━━━━━━━━━━━━━┛", 0
+
+    panel_help:
+        db 8
+        db 56
+        db 8
+        db "┏━━━━━━━Help━━━━━━━┓", 0
+        db "┃ Left      h, ←   ┃", 0
+        db "┃ Right     l, →   ┃", 0
+        db "┃ Down      j, ↓   ┃", 0
+        db "┃ Rotate    k, ↑   ┃", 0
+        db "┃ Drop      space  ┃", 0
+        db "┃ Quit      q      ┃", 0
+        db "┗━━━━━━━━━━━━━━━━━━┛", 0
+
+    panel_main:
+        db 22
+        db 25
+        db 1
+        db "┏━━━━━━━━━━━Tetris━━━━━━━━━━━┓", 0
+        db "┃                            ┃", 0  ;# Row 20 (Top)
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0 
+        db "┃                            ┃", 0  ;# Row 1 (Bottom)
+        db "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛", 0
 
 
 section .text
@@ -29,43 +92,80 @@ setup_board:
     mov rdx, clear_len
     syscall
 
-    call _print_test
+    mov rdi, panel_stats
+    call _draw_panel
+
+    mov rdi, panel_next
+    call _draw_panel
+
+    mov rdi, panel_help
+    call _draw_panel
+
+    mov rdi, panel_main
+    call _draw_panel
+
     ret
 
 
 ; =======  Helpers  ======================================================== #
 
-_print_test:
-    mov rdi, hello_world
-    mov rsi, hello_world_len
-    mov rdx, 0
-    mov r10, 0
-    call write_to_screen
+; Draws a given panel (multi-line elem) on the screen at (X,Y), where (1,1) is
+; the top-left corner. Renders each line of the panel STARTING at the
+; specified coordinates.
+; Arguments:
+;   rdi - Pointer to panel data: first 3 bytes are metadata (height, x-offset, 
+;         y-offset), followed by height null-terminated string rows
+; Return:
+;   rax - Success (0) or error (-1)
+_draw_panel:
+    ; === PROLOGUE ===
+    push r15
+    push r14
+    push r13
+    push r12
 
-    test rax, rax
-    jne .error
+    xor r12, r12
+    xor r13, r13
+    xor r14, r14
+    xor r15, r15
 
-    lea rdi, [hello_world_2]
-    mov rsi, hello_world_len
-    mov rdx, 7
-    mov r10, 7
-    call write_to_screen
+    mov r12b, byte [rdi]        ; height
+    mov r13b, byte [rdi+1]      ; col (X)
+    mov r14b, byte [rdi+2]      ; line (Y)
+    add r12b, r14b              ; end_Y = height + starting line (Y)
+    lea r15, [rdi+3]            ; data
+    
+    .print_lines:
+        mov rdi, r15
+        mov rsi, 1024
+        call strlen
 
-    test rax, rax
-    jne .error
+        cmp rax, 1024
+        je .error
 
-    lea rdi, [hello_world_3]
-    mov rsi, hello_world_len
-    mov rdx, 3
-    mov r10, 3
-    call write_to_screen
+        mov rdi, r15
+        mov rsi, rax
+        mov rdx, r13
+        mov r10, r14
+        call write_to_screen
 
-    test rax, rax
-    jne .error
+        lea r15, [r15+rax+1]    ; next line
+        inc r14
 
-    ret
+        cmp r14, r12
+        jb .print_lines         ; not all lines printed?
+
+    mov rax, 0
+    jmp .return
 
     .error:
-        mov rdi, rax
-        mov rax, 60
-        syscall
+        mov rax, 1
+
+    .return:
+        pop r12
+        pop r13
+        pop r14
+        pop r15
+        pop rbp
+
+        ret
--- src/utils.s
+++ src/utils.s
@@ -1,19 +1,19 @@
-global write_to_screen
+global write_to_screen, strlen
 default rel
 
 
 section .text
 
-; Writes the given str to the screen at (X,Y), where (0,0) is the top-left
+; Writes the given str to the screen at (X,Y), where (1,1) is the top-left
 ; corner. We'll use the `\x1b[Y;XH` cursor control sequence to position it.
-; Errors out when X and/or Y > 999 or < 0
+; Errors out when X and/or Y > 999 or < 1
 ; Arguments:
 ;   rdi - Pointer to the str buffer
 ;   rsi - Length of the str
 ;   rdx - X as an integer
 ;   r10 - Y as an integer
 ; Return:
-;   rax - Success (0) or error (1)
+;   rax - Number of bytes written
 write_to_screen:
     ; Terminal dimensions don't realistically exceed (999, 999)
     cmp rdx, 999
@@ -21,58 +21,96 @@ write_to_screen:
     cmp r10, 999
     ja .error
 
-    cmp rdx, 0
+    cmp rdx, 1
     jb .error
-    cmp r10, 0
+    cmp r10, 1
     jb .error
 
     ; === PROLOGUE ===
     ; Stack frame for the function
     push rbp
     mov rbp, rsp
-    sub rsp, 11                 ; X (3B) + Y (3B) + `\x1b`, `[`, `;`, `H` + \0
+    sub rsp, 10                 ; X (3B) + Y (3B) + `\x1b`, `[`, `;`, `H`
 
-    push rsi                    ; Save original string length
-    push rdi                    ; Save original string pointer
+    ; Save originals for later
+    push rsi
+    push rdi 
+    push rdx
 
-    mov byte [rbp-11], `\x1b`
-    mov byte [rbp-10], `[`
+    mov byte [rbp-10], `\x1b`
+    mov byte [rbp-9], `[`
 
     mov rdi, r10                ; Y coordinate
-    lea rsi, [rbp-9]
+    lea rsi, [rbp-8]
     call _itoa
 
     mov r10, rax                ; Y str len
-    mov byte [rbp-9+r10], `;`
+    mov byte [rbp-8+r10], `;`
 
-    mov rdi, rdx                ; X coordinate
-    lea rsi, [rbp-8+r10]        ; 8 accounts for the `;`
+    pop rdi                     ; X coordinate (rdx in stack)
+    lea rsi, [rbp-7+r10]        ; 8 accounts for the `;`
     call _itoa
 
     add r10, rax                ; Y + X str len
-    mov byte [rbp-8+r10], `H`
+    mov byte [rbp-7+r10], `H`
 
     ; Print the ANSI escape string to position cursor
-    lea rdi, [rbp-11]
-    lea rsi, [r10+5]
-    xor rdx, rdx
-    call _write_to_stdout
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rbp-10]
+    lea rdx, [r10+4]
+    syscall
+
+    cmp rax, rdx                ; null terminator is not written
+    jne .error
 
     ; Print the actual string
-    pop rdi
+    mov rax, 1
+    mov rdi, 1
     pop rsi
-    xor rdx, rdx
-    call _write_to_stdout
+    pop rdx
+    syscall
 
     ; Restore stack
     mov rsp, rbp
     pop rbp
 
-    mov rax, 0
     ret
 
     .error:
-        mov rax, 1
+        mov rax, 0
+        ret
+
+; Returns the byte-length of a given string up-to 1024 bytes. If the string has
+; multi-byte UTF-8 characters, each byte is counted individually.
+; Arguments:
+;   rdi - Pointer to the null-terminated string buffer
+;   rsi - Maximum number of characters to examine
+; Return:
+;   rax - Byte-length of the string (excluding null terminator), or zero if str
+;         is a null pointer, and rsi if the null character was not found in the
+;         first rsi bytes.
+strlen:
+    xor eax, eax            ; al = 0
+    test rdi, rdi
+    jz .done
+
+    cld                     ; Ensure we are scanning FORWARD
+    mov rcx, rsi
+
+    repne scasb             ; Scan while [rdi] != al AND rcx != 0
+
+    xor rdi, rdi
+    setz dil                ; Save og ZF flag
+
+    mov rax, rsi
+    sub rax, rcx            ; rax = bytes_scanned
+    
+    test dil, dil
+    jz .done                ; ZF=0 -> no null found, we hit limit -> rax == rsi
+    dec rax                 ; ZF=1 -> found null -> exclude null from length
+
+    .done:
         ret
 
 
@@ -115,49 +153,3 @@ _itoa:
 
     mov byte [rsi + r8], 0x0
     ret
-
-; Writes the given str to stdout with a new line
-; Arguments:
-;   rdi - Pointer to the str buffer
-;   rsi - Length
-;   rdx - Wether to print newline (1) or not (0)
-; Return:
-;   rax - Success (0) or error (1)
-_write_to_stdout:
-    mov r8, rdi
-    mov r9, rsi
-    xor r10, r10
-
-    test rdx, rdx
-    jz .write_it
-
-    cmp rdx, 1
-    je .write_it
-
-    mov rax, 1          ; Error
-    jmp .return
-
-    .write_it:
-        mov rdi, 1
-        mov rsi, r8
-        mov rdx, r9
-        mov rax, 1
-        syscall
-
-        test r10, r10
-        jz .return
-
-        dec rsp
-        mov byte [rsp], `\n`
-
-        mov rdi, 1
-        mov rsi, rsp
-        mov rdx, 1
-        mov rax, 1
-        syscall
-
-        inc rsp
-        mov rax, 0      ; Success
-
-    .return:
-        ret
