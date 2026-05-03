3ac313636584fd4fb8dae7d8ab9853c9710390ab
m1urah <173448736+m1urah@users.noreply.github.com>
2026-05-03 18:48:11 +0200

    Various refactors, add piece movement logic, and remove test func
    Refactor to follow the Model-View-Controller (MVC) pattern:
    
    - Move screen rendering logic into screen.s
    - Move screen-only utils into screen.s
    - Keep game-only logic into game.s

--- src/game.s
+++ src/game.s
@@ -1,19 +1,11 @@
 global process_input, print_board, test_board
-extern write_to_screen
+global GAME_BOARD_WIDTH
 default rel
 
+GAME_BOARD_WIDTH        equ 13
+GAME_BOARD_HEIGHT       equ 20
+
 section .rodata
-    ; === BOARD ===
-    game_board_dim:
-        db 13           ; Board Width
-        db 21           ; Board height
-
-    terminal_board_dim:
-        db 26           ; Board Width
-        db 20           ; Board height
-        db 26           ; Initial X
-        db 2            ; Initial Y
-
     ; === TETROMINOS ===
     ; Following the Super Rotation System (SRS), the I piece uses a 4x4
     ; bouding box, J, L, S, T, Z pieces 3x3, and the O piece 2x2.
@@ -67,13 +59,28 @@ section .rodata
 
 section .data
     ; Logical game board. Possible values:
-    ; - 0x20: empty cell (space)
-    ; - o, i, s, z, l, j, t: identifies part of a piece. Represented as:
-    ;     oo  iiii   ss  zz     l  j      t 
-    ;     oo        ss    zz  lll  jjj  ttt
-    game_board              times 273 db 0x20       ; 13 cols x 21 lines
+    ;   - 0x20: empty cell (space)
+    ;   - o, i, s, z, l, j, t: identifies part of a piece. Represented as:
+    ;       oo  iiii   ss  zz     l  j      t 
+    ;       oo        ss    zz  lll  jjj  ttt
+    game_board              times 260 db 0x20       ; 13 cols x 20 lines
     game_board_len          equ $-game_board
 
+    spawn_piece             db 0                    ; Wether to spawn (1) a new piece on the board
+
+section .bss
+    ; Tracks the active piece:
+    ;   - Offset 0: Width (cols)
+    ;   - Offset 1: Height (rows)
+    ;   - Offset 2: X position
+    ;   - Offset 3: Y position
+    ;   - Offset 4: Piece character
+    ;   - Offset 5: Array length
+    ;   - Offset 6: Array data (1 for solid, 0 for empty). Size defined by Offset 4
+    active_piece            resb 22     ; Up to 22 chars (piece_i)
+
+; TODO: When adding a new piece, verify both width and height are at least one (or
+; that they match one of the figures)
 
 section .text
 
@@ -117,104 +124,193 @@ process_input:
     .do_up:
     .do_down:
     .done:
-        ret
 
-test_board:
-    %assign i 0
-    %assign char 'a'
 
-    %rep 20
-        mov byte [game_board + i], char
-        %assign i i + 13
-        %assign char char + 1
-    %endrep
+; =======  Piece Movement  ================================================== #
 
+; Moves the active piece one position (right, left, or down). (0, 0) is not a
+; valid move. This function orchestrates the whole flow:
+;   1. Erase the piece from its current position
+;   2. Call can_move(dx, dy)
+;   3. If yes, update x_pos/y_pos
+;   4. Move the piece at the (possible new) position
+; Arguments:
+;   rdi - Vertical displacement (dy): 1 for Down, 0 for horizontal moves
+;   rsi - Horizontal displacement (dx): -1 for Left, 1 for Right, 0 for Down
+; Return:
+;   rax - Whether the piece was moved (1) or not (0)   
+move_piece:
+    push r15
+    push r14
+    push r13
+
+    mov r15, rdi
+    mov r14, rsi
+    mov r13, 1                              ; Piece was moved
+
+    call _erase_piece
+
+    mov rdi, r15
+    mov rsi, r14
+    call _can_move_piece
+
+    test rax, rax
+    jnz .draw_piece
+
+    ; Put piece in original position
+    mov r15, 0
+    mov r14, 0
+    mov r13, 0                              ; Piece was NOT moved
+
+    .draw_piece:
+        ; Update coordinates
+        add r14b, byte [active_piece + 2]   ; X
+        add r15b, byte [active_piece + 3]   ; Y
+        mov [active_piece + 2], r14b
+        mov [active_piece + 3], r15b
+
+        movzx edi, [active_piece + 4]       ; Piece's char
+        call _modify_piece_on_board
+
+    mov rax, r13
+    pop r13
+    pop r14
+    pop r15
     ret
 
-; Updates the screen with the logical board. Each byte in game_board represents
-; two in the screen_board.
+; Erases a piece from its current position. Allows us to verify if the piece
+; can be moved to a (possible new) location without colliding with itself.
 ; Arguments:
 ;   None
 ; Return:
+;   None 
+_erase_piece:
+    mov edi, 0x20
+    call _modify_piece_on_board
+
+; Iterates through the active piece's array and writes a specified character to
+; the corresponding coords on the game board. This helper is used for both
+; setting the piece and erasing it.
+; Arguments:
+;   rdi - The char to write to the board: 0x20 to erase, or the piece's specific
+;         character.
+; Returns:
 ;   None
-print_board:
-    movzx r15, byte [terminal_board_dim + 2]    ; Initial X
-    movzx r14, byte [terminal_board_dim + 3]    ; Initial Y
+_modify_piece_on_board:
+    ; Using r32 as is 1-byte smaller in machine code and achieves the same
+    movzx r8d, byte [active_piece]       ; Piece width
+    movzx r9d, byte [active_piece + 1]   ; Piece height
+    movzx r10d, byte [active_piece + 2]  ; Piece X pos
+
+    ; Just a label plus a constant displacement, with no base/index registers,
+    ; NASM will automatically compile this as a RIP-relative load.
+    lea r11, [active_piece + 6]         ; src pointer
+
+    movzx ecx, byte [active_piece + 3]
+    imul eax, ecx, GAME_BOARD_WIDTH
+    add rax, r10                        ; rax = (y * board_width) + x
+
+    lea rcx, [game_board]               ; RIP-relative load
+    add rax, rcx                        ; dst pointer = game_board[rax]
+
+    .outer_loop_start:
+        mov rcx, r8                     ; Reset width
+
+        .inner_loop_start:
+            cmp byte [r11], 0
+            jz .next_col
+
+            mov byte [rax], dil         ; Set that piece's cell
+
+            .next_col:
+                inc r11
+                inc rax
+                dec rcx
+                jnz .inner_loop_start   ; If dec makes rcx = 0, ZF is set to 1
+
+    .outer_loop_end:
+        sub rax, r8                     ; Reset dst ptr X
+        lea rax, [rax + GAME_BOARD_WIDTH]
+        dec r9
+        jnz .outer_loop_start
     
-    mov r13, r14
-    add r13b, byte [terminal_board_dim + 1]     ; End Y = Y + height
-
-    lea r12, [game_board]                       ; src ptr
-
-    .print_next_line:
-        cmp r14, r13
-        jae .return
-
-        mov rdi, r12
-        mov rsi, r15
-        mov rdx, r14
-        call _print_board_line
-
-        ; Next line
-        inc r14
-        lea r12, [r12 + rax]
-
-        jmp .print_next_line
-
     .return:
         ret
 
-
-; =======  Helpers  ======================================================== #
-
-; Prints a line from the logical board into the screen. Lines width are
-; by their respective widths:
-;   - Logical board: [game_board_dim]
-;   - Terminal board: [terminal_board_dim ]
+; Check if the active piece can be moved by one position (right, left, or
+; down). (0, 0) is not a valid move.
+; This assumes the active piece have been erased from the board BEFORE it
+; was called.
 ; Arguments:
-;   rdi - Pointer to the logical board
-;   rsi - X coordinate
-;   rdx - Y coordinate
+;   rdi - Vertical displacement (dy): 1 for Down, 0 for horizontal moves
+;   rsi - Horizontal displacement (dx): -1 for Left, 1 for Right, 0 for Down
 ; Return:
-;   rax - Number of bytes written
-_print_board_line:
-    push r15
-    movzx r15, byte [terminal_board_dim]     ; buffer size
-    movzx rcx, byte [game_board_dim]        ; count (width)
+;   rax - Whether the piece can be moved (1) or not (0)   
+_can_move_piece:
+    cmp rdi, rsi                        ; Invalid move
+    je .false
+    cmp rdi, 0
+    jl .false
 
-    sub rsp, r15
+    movzx r8d, byte [active_piece + 2]
+    add r8, rsi                         ; target_x = x + dx
+    movzx r9d, byte [active_piece + 3]
+    add r9, rdi                         ; target_y = y + dy
 
-    mov r10, rsp                    ; dst pointer
-    mov r11, rdi                    ; src pointer
+    ; Check left, right, down (there's no up)
+    cmp r8, GAME_BOARD_WIDTH
+    ja .false                           ; Unsigned check, also evaluates < 0 (two's complement -> 0xFFF..)
+    cmp r9, GAME_BOARD_HEIGHT
+    ja .false
+    
+    movzx ecx, byte [active_piece]      ; Piece width
+    movzx edx, byte [active_piece + 1]  ; Piece height
 
-    .dup_byte:
-        test rcx, rcx
-        jz .done
+    ; Now check opposite coords (bottom-right corner)
+    lea r10, [r8 + rcx]
+    cmp r10, GAME_BOARD_WIDTH
+    ja .false
 
-        mov al, [r11]
-        mov ah, al                  ; dup byte        
-        mov [r10], ax
+    lea r10, [r9 + rdx]
+    cmp r10, GAME_BOARD_HEIGHT
+    ja .false
 
-        inc r11
-        add r10, 2
-        dec rcx
-        jmp .dup_byte
+    lea r10, [active_piece + 6]         ; src pointer
 
-    .done:
-        mov rbx, r11
-        sub rbx, rdi                ; We return this (bytes read: src_end - src_start)
+    imul r11d, r9d, GAME_BOARD_WIDTH
+    add r11, r8                         ; (target_y * board_width) + target_x
+    lea rax, [game_board]
+    add r11, rax                        ; dst pointer = game_board[r11]
 
-        mov rdi, rsp
-        mov rax, r10
-        mov r10, rdx
-        mov rdx, rsi
+    .outer_loop_start:
+        mov rax, rcx                    ; Reset width
 
-        mov rsi, rax
-        sub rsi, rsp                ; length = dst_end - dst_start
-        call write_to_screen
-        
-        add rsp, r15                ; Restore stack
-        pop r15
+        .inner_loop_start:
+            cmp byte [r10], 0
+            jz .next_col
 
-        mov rax, rbx
+            cmp byte [r11], 0x20
+            jne .false
+
+            .next_col:
+                inc r10
+                inc r11
+                dec rax
+                jnz .inner_loop_start
+
+    .outer_loop_end:
+        sub r11, rcx                    ; Reset dst ptr X
+        lea r11, [r11 + GAME_BOARD_WIDTH]
+        dec rdx
+        jnz .outer_loop_start
+
+    .true:
+        mov rax, 1
+        jmp .return
+
+    .false:
+        mov rax, 0
+    
+    .return:
         ret
+
--- src/screen.s
+++ src/screen.s
@@ -1,6 +1,11 @@
 global init_board
-extern write_to_screen, strlen
+extern GAME_BOARD_WIDTH
 default rel
+
+TERMINAL_BOARD_WIDTH    equ 26
+TERMINAL_BOARD_HEIGHT   equ 20
+TERMINAL_BOARD_INIT_X   equ 26
+TERMINAL_BOARD_INIT_Y   equ 26
 
 section .rodata
     ;# Enter alternate buffer -> clear screen -> hide cursor
@@ -17,7 +22,7 @@ section .rodata
     panel_stats:
         db 5    ; height
         db 1    ; col (X)
-        db 1    ; line(Y)
+        db 1    ; line (Y)
         ; utf-8 chars are multi-byte, null terminator marks end of row
         db "┏━━━━━━━Stats━━━━━━━┓", 0
         db "┃ Score       10420 ┃", 0
@@ -26,9 +31,7 @@ section .rodata
         db "┗━━━━━━━━━━━━━━━━━━━┛", 0
 
     panel_next:
-        db 5
-        db 56
-        db 1
+        db 5, 56, 1
         db "┏━━━━━━━Next━━━━━━━┓", 0
         db "┃                  ┃", 0
         db "┃     ████████     ┃", 0
@@ -36,9 +39,7 @@ section .rodata
         db "┗━━━━━━━━━━━━━━━━━━┛", 0
 
     panel_help:
-        db 8
-        db 56
-        db 8
+        db 8, 56, 8
         db "┏━━━━━━━Help━━━━━━━┓", 0
         db "┃ Left      h, ←   ┃", 0
         db "┃ Right     l, →   ┃", 0
@@ -49,9 +50,7 @@ section .rodata
         db "┗━━━━━━━━━━━━━━━━━━┛", 0
 
     panel_main:
-        db 22
-        db 24
-        db 1
+        db 22, 24, 1
         db "┏━━━━━━━━━━━Tetris━━━━━━━━━━━┓", 0
         db "┃                            ┃", 0  ;# Row 20 (Top)
         db "┃                            ┃", 0
@@ -77,6 +76,9 @@ section .rodata
 
 
 section .text
+
+
+; =======  Board Initialization  ============================================ #
 
 ; Setups the board in the alternate buffer by cleaning the screen,
 ; and placing all initial panels.
@@ -105,9 +107,6 @@ init_board:
     call _draw_panel
 
     ret
-
-
-; =======  Helpers  ======================================================== #
 
 ; Draws a given panel (multi-line elem) on the screen at (X,Y), where (1,1) is
 ; the top-left corner. Renders each line of the panel STARTING at the
@@ -138,7 +137,7 @@ _draw_panel:
     .print_lines:
         mov rdi, r15
         mov rsi, 1024
-        call strlen
+        call _strlen
 
         cmp rax, 1024
         je .error
@@ -168,3 +167,246 @@ _draw_panel:
         pop r15
 
         ret
+
+; Returns the byte-length of a given string up-to 1024 bytes. If the string has
+; multi-byte UTF-8 characters, each byte is counted individually.
+; Arguments:
+;   rdi - Pointer to the null-terminated string buffer
+;   rsi - Maximum number of characters to examine
+; Return:
+;   rax - Byte-length of the string (excluding null terminator), or zero if str
+;         is a null pointer, and rsi if the null character was not found in the
+;         first rsi bytes.
+_strlen:
+    xor eax, eax            ; al = 0
+    test rdi, rdi
+    jz .done
+
+    cld                     ; Ensure we are scanning FORWARD
+    mov rcx, rsi
+
+    repne scasb             ; Scan while [rdi] != al AND rcx != 0
+
+    xor rdi, rdi
+    setz dil                ; Save og ZF flag
+
+    mov rax, rsi
+    sub rax, rcx            ; rax = bytes_scanned
+    
+    test dil, dil
+    jz .done                ; ZF=0 -> no null found, we hit limit -> rax == rsi
+    dec rax                 ; ZF=1 -> found null -> exclude null from length
+
+    .done:
+        ret
+
+
+; =======  Board Rendering  ================================================= #
+
+; Updates the screen with the logical board. To scale the visual output, each
+; byte from the buffer is duplicated horizontally when rendered.
+; Arguments:
+;   rdi - Pointer to the start of the game_board buffer
+; Return:
+;   None
+print_board:
+    movzx r15, TERMINAL_BOARD_INIT_X        ; Initial X
+    movzx r14, TERMINAL_BOARD_INIT_Y        ; Initial Y
+    
+    mov r13, r14
+    add r13b, TERMINAL_BOARD_HEIGHT         ; End Y = Y + height
+
+    lea r12, rdi                            ; src ptr
+
+    .print_next_line:
+        cmp r14, r13
+        jae .return
+
+        mov rdi, r12
+        mov rsi, r15
+        mov rdx, r14
+        call _print_board_line
+
+        ; Next line
+        inc r14
+        lea r12, [r12 + rax]
+
+        jmp .print_next_line
+
+    .return:
+        ret
+
+; Prints a single line from the game board buffer to the screen. To scale the
+; visual output, each byte from the buffer is duplicated horizontally when
+; rendered.
+; Lines width are by their respective widths:
+;   - Logical board: GAME_BOARD_WIDTH
+;   - Terminal board: TERMINAL_BOARD_WIDTH
+; Arguments:
+;   rdi - Pointer to the start of the game_board buffer line
+;   rdx - X as an integer
+;   r10 - Y as an integer
+_print_board_line:
+    push r15
+    movzx r15, TERMINAL_BOARD_WIDTH ; buffer size
+    movzx rcx, GAME_BOARD_WIDTH     ; count (width)
+
+    sub rsp, r15
+
+    mov r10, rsp                    ; dst pointer
+    mov r11, rdi                    ; src pointer
+
+    .dup_byte:
+        test rcx, rcx
+        jz .done
+
+        mov al, [r11]
+        mov ah, al                  ; dup byte        
+        mov [r10], ax
+
+        inc r11
+        add r10, 2
+        dec rcx
+        jmp .dup_byte
+
+    .done:
+        mov rbx, r11
+        sub rbx, rdi                ; We return this (bytes read: src_end - src_start)
+
+        mov rdi, rsp
+        mov rax, r10
+        mov r10, rdx
+        mov rdx, rsi
+
+        mov rsi, rax
+        sub rsi, rsp                ; length = dst_end - dst_start
+        call write_to_screen
+        
+        add rsp, r15                ; Restore stack
+        pop r15
+
+        mov rax, rbx
+        ret
+
+
+; =======  Utils  ======================================================== #
+
+; Writes the given str to the screen at (X,Y), where (1,1) is the top-left
+; corner. We'll use the `\x1b[Y;XH` cursor control sequence to position it.
+; Errors out when X and/or Y > 999 or < 1
+; Arguments:
+;   rdi - Pointer to the str buffer
+;   rsi - Length of the str
+;   rdx - X as an integer
+;   r10 - Y as an integer
+; Return:
+;   rax - Number of bytes written
+write_to_screen:
+    push rbx
+    mov rbx, r10
+
+    ; Terminal dimensions don't realistically exceed (999, 999)
+    cmp rdx, 999
+    ja .error
+    cmp rbx, 999
+    ja .error
+
+    cmp rdx, 1
+    jb .error
+    cmp rbx, 1
+    jb .error
+
+    ; === PROLOGUE ===
+    ; Stack frame for the function
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10                 ; X (3B) + Y (3B) + `\x1b`, `[`, `;`, `H`
+
+    ; Save originals for later
+    push rsi
+    push rdi 
+    push rdx
+
+    mov byte [rbp-10], `\x1b`
+    mov byte [rbp-9], `[`
+
+    mov rdi, rbx                ; Y coordinate
+    lea rsi, [rbp-8]
+    call _itoa
+
+    mov rbx, rax                ; Y str len
+    mov byte [rbp-8+rbx], `;`
+
+    pop rdi                     ; X coordinate (rdx in stack)
+    lea rsi, [rbp-7+rbx]        ; 8 accounts for the `;`
+    call _itoa
+
+    add rbx, rax                ; Y + X str len
+    mov byte [rbp-7+rbx], `H`
+
+    ; Print the ANSI escape string to position cursor
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rbp-10]
+    lea rdx, [rbx+4]
+    syscall
+
+    cmp rax, rdx                ; null terminator is not written
+    jne .error
+
+    ; Print the actual string
+    mov rax, 1
+    mov rdi, 1
+    pop rsi
+    pop rdx
+    syscall
+
+    ; Restore stack
+    mov rsp, rbp
+    pop rbp
+
+    pop rbx
+    ret
+
+    .error:
+        pop rbx
+        mov rax, 0
+        ret
+
+; Converts an integer into its ASCII representation
+; Arguments:
+;   rdi - Integer
+;   rsi - Pointer to the caller-allocated buffer
+; Return:
+;   rax - Length of the generated string
+_itoa:
+    mov rax, rdi
+    mov r9, 10
+    xor r10, r10          ; str ptr
+    xor r11, r11          ; Pushed digits
+
+    .get_digits:
+        xor rdx, rdx
+        div r9
+
+        push rdx
+        inc r11
+
+        test rax, rax
+        jnz .get_digits
+
+    mov rax, r11         ; str len (ret value)
+
+    .digit_to_ascii:
+        pop r9         ; Retrieves digits in correct order
+        dec r11
+
+        lea rcx,  [r9 + '0']
+        mov byte [rsi + r10], cl
+        inc r10
+
+        test r11, r11
+        jnz .digit_to_ascii
+
+    mov byte [rsi + r10], 0x0
+    ret
