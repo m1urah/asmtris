df80fca5c5113756a413937bbd0a47601c3761c4
m1urah <173448736+m1urah@users.noreply.github.com>
2026-05-05 20:42:20 +0200

    Add piece rotation functionality, improve movement logic, and account for negative coordinates

--- src/game.s
+++ src/game.s
@@ -79,7 +79,7 @@ section .bss
     ; Tracks the active piece:
     ;   - Offset 0: Width (cols)
     ;   - Offset 1: Height (rows)
-    ;   - Offset 2: X position
+    ;   - Offset 2: X position (might be negative to account for empty cols)
     ;   - Offset 3: Y position
     ;   - Offset 4: Piece character
     ;   - Offset 5: Array length
@@ -126,6 +126,10 @@ process_input:
     cmp eax, `\e[B`
     je .do_down
 
+    .do_up:     ; rotate
+        jmp rotate_figure
+        ret
+
     .do_left:   ; (0, -1)
         mov rdi, 0
         mov rsi, -1
@@ -135,9 +139,6 @@ process_input:
         mov rdi, 0
         mov rsi, 1
         jmp .apply_move
-
-    .do_up:     ; TBD -> rotate
-        jmp .done
 
     .do_down:   ; (1, 0)
         mov rdi, 1
@@ -252,7 +253,7 @@ move_piece:
     mov r14, rsi
     mov r13, 1                              ; Piece was moved
 
-    call _erase_piece
+    call _erase_piece_from_board
 
     mov rdi, r15
     mov rsi, r14
@@ -268,8 +269,11 @@ move_piece:
 
     .draw_piece:
         ; Update coordinates
-        add r14b, byte [active_piece + 2]   ; X
-        add r15b, byte [active_piece + 3]   ; Y
+        ; sx needed, coordinates might be negative!!!
+        movsx r8, byte [active_piece + 2]   
+        add r14, r8                         ; X
+        movsx r8, byte [active_piece + 3]
+        add r15, r8                         ; Y
         mov [active_piece + 2], r14b
         mov [active_piece + 3], r15b
 
@@ -288,7 +292,7 @@ move_piece:
 ;   None
 ; Return:
 ;   None 
-_erase_piece:
+_erase_piece_from_board:
     mov edi, 0x20
     call _modify_piece_on_board
 
@@ -302,16 +306,16 @@ _erase_piece:
 ;   None
 _modify_piece_on_board:
     ; Using r32 as is 1-byte smaller in machine code and achieves the same
-    movzx r8d, byte [active_piece]       ; Piece width
-    movzx r9d, byte [active_piece + 1]   ; Piece height
-    movzx r10d, byte [active_piece + 2]  ; Piece X pos
+    movzx r8d, byte [active_piece]      ; Piece width
+    movzx r9d, byte [active_piece + 1]  ; Piece height
+    movsx r10, byte [active_piece + 2]  ; Piece X pos
 
     ; Just a label plus a constant displacement, with no base/index registers,
     ; NASM will automatically compile this as a RIP-relative load.
     lea r11, [active_piece + 6]         ; src pointer
 
-    movzx ecx, byte [active_piece + 3]
-    imul eax, ecx, GAME_BOARD_WIDTH
+    movsx rcx, byte [active_piece + 3]
+    imul rax, rcx, GAME_BOARD_WIDTH
     add rax, r10                        ; rax = (y * board_width) + x
 
     lea rcx, [game_board]               ; RIP-relative load
@@ -321,7 +325,7 @@ _modify_piece_on_board:
         mov rcx, r8                     ; Reset width
 
         .inner_loop_start:
-            cmp byte [r11], 0
+            cmp byte [r11], 0 
             jz .next_col
 
             mov byte [rax], dil         ; Set that piece's cell
@@ -351,60 +355,200 @@ _modify_piece_on_board:
 ; Return:
 ;   rax - Whether the piece can be moved (1) or not (0)   
 _can_move_piece:
+    mov rax, 0
     cmp rdi, rsi                        ; Invalid move
-    je .false
+    je .return
     cmp rdi, 0
-    jl .false
+    jl .return
 
-    movzx r8d, byte [active_piece + 2]
+    movsx r8, byte [active_piece + 2]
     add r8, rsi                         ; target_x = x + dx
-    movzx r9d, byte [active_piece + 3]
+    movsx r9, byte [active_piece + 3]
     add r9, rdi                         ; target_y = y + dy
 
-    ; Check left, right, down (there's no up)
-    cmp r8, GAME_BOARD_WIDTH
-    ja .false                           ; Unsigned check, also evaluates < 0 (two's complement -> 0xFFF..)
-    cmp r9, GAME_BOARD_HEIGHT
-    ja .false
-    
+    mov rdi, r8
+    mov rsi, r9
+    call _can_place_piece
+
+    .return:
+        ret
+
+
+; =======  Piece Rotation  ================================================== #
+
+; Rotates the current active figure 90 degrees clockwise. To verify this:
+;   1. Erase the piece from its current position
+;   2. Rotate piece
+;   3. Check if the rotated piece can be placed at the current (X, Y) coords
+;   4. If not, revert rotation
+;   5. Draw piece
+; Arguments:
+;   None
+; Return:
+;   rax - Whether the piece was rotated (1) or not (0)   
+rotate_figure:
+    push r15
+
+    call _erase_piece_from_board
+
+    ; Backup the original figure array
+    mov rax, qword [active_piece+6]
+    mov rcx, qword [active_piece+14]
+    push rcx
+    push rax
+
+    lea rdi, [active_piece+6]
+    movzx esi, byte [active_piece]
+    call _rotate_array
+
+    movsx rdi, byte [active_piece+2]
+    movsx rsi, byte [active_piece+3]
+    call _can_place_piece
+
+    test rax, rax
+    mov r15, rax
+
+    pop rax
+    pop rcx
+    jnz .draw_piece
+
+    ; Restore original figure array
+    mov qword [active_piece+6], rax
+    mov qword [active_piece+14], rcx
+
+    .draw_piece:
+        movzx edi, byte [active_piece + 4]  ; Piece's char
+        call _modify_piece_on_board
+
+    mov rax, r15
+    pop r15
+
+    ret
+
+; Rotates a square matrix 90 degrees clockwise. The process is as follows:
+;   1. Transpose matrix by swapping the elements at index (i,j) with the
+;      elements at (j,i): rows --> columns
+;   2. Reverse the order of elements in every row.
+; Arguments:
+;   rdi - Pointer to the array containing the matrix
+;   rsi - Number of cols/rows
+; Return:
+;   None
+_rotate_array:
+    lea rax, [rsi - 1]
+    xor r8, r8              ; i (row)
+    xor r9, r9              ; j (col)
+
+    ; 1. Transpose
+    .outer_loop_start_t:
+        mov r9, r8          ; j (col) = i + 1
+        inc r9              ; (i, j) where i = j never changes, skip it
+
+        ; Calculate offsets
+        mov r10, r8
+        imul r10, rsi
+        add r10, r9         ; (i, j) = (i x height) + j
+
+        mov r11, r9
+        imul r11, rsi
+        add r11, r8         ; (j, i) = (j x width) + i
+
+        .inner_loop_t:
+            movzx ecx, byte [rdi + r10]
+            movzx edx, byte [rdi + r11]
+
+            mov byte [rdi + r10], dl
+            mov byte [rdi + r11], cl
+
+            inc r9
+            inc r10
+            add r11, rsi
+            cmp r9, rsi
+            jb .inner_loop_t
+
+        inc r8
+        cmp r8, rax
+        jb .outer_loop_start_t 
+
+    ; 2. Reverse rows
+    xor r10, r10            ; current row
+    mov r11, rsi
+    imul r11, r11           ; max row offset
+    .outer_loop_start_r:
+        lea r8, [rdi + r10] ; ptr row start
+        lea r9, [r8 + rax]  ; ptr row end
+
+        .inner_loop_r:
+            movzx ecx, byte [r8]
+            movzx edx, byte [r9]
+
+            mov byte [r8], dl
+            mov byte [r9], cl
+
+            inc r8
+            dec r9
+            cmp r8, r9
+            jb .inner_loop_r
+
+        add r10, rsi
+        cmp r10, r11
+        jb .outer_loop_start_r
+
+    ret
+
+
+; =======  Helpers  ========================================================= #
+
+; Checks if the active piece can be placed at the specified (X, Y) coordinates
+; without hitting the walls or other blocks.
+; Arguments:
+;   rdi - Target X coordinate (column)
+;   rsi - Target Y coordinate (row)
+; Return:
+;   rax - 1 (True) if it can be placed, 0 (False) if not.
+_can_place_piece:
     movzx ecx, byte [active_piece]      ; Piece width
     movzx edx, byte [active_piece + 1]  ; Piece height
 
-    ; Now check opposite coords (bottom-right corner)
-    lea r10, [r8 + rcx]
-    cmp r10, GAME_BOARD_WIDTH
-    ja .false
+    lea r8, [active_piece + 6]          ; src pointer
 
-    lea r10, [r9 + rdx]
-    cmp r10, GAME_BOARD_HEIGHT
-    ja .false
+    imul r9d, esi, GAME_BOARD_WIDTH
+    add r9, rdi                         ; (target_y * board_width) + target_x
+    lea r10, [game_board]
+    add r9, r10                         ; dst pointer = game_board[r11]
 
-    lea r10, [active_piece + 6]         ; src pointer
-
-    imul r11d, r9d, GAME_BOARD_WIDTH
-    add r11, r8                         ; (target_y * board_width) + target_x
-    lea rax, [game_board]
-    add r11, rax                        ; dst pointer = game_board[r11]
+    mov r10, rdi                        ; x
+    mov r11, rsi                        ; y
 
     .outer_loop_start:
         mov rax, rcx                    ; Reset width
+        mov r10, rdi
 
         .inner_loop_start:
-            cmp byte [r10], 0
+            cmp byte [r8], 0
             jz .next_col
 
-            cmp byte [r11], 0x20
+            cmp r10, GAME_BOARD_WIDTH
+            jae .false                   ; Unsigned check, also evaluates < 0 (two's complement -> 0xFFF..)
+            cmp r11, GAME_BOARD_HEIGHT
+            jae .false
+
+            cmp byte [r9], 0x20
             jne .false
 
             .next_col:
+                inc r8
+                inc r9
                 inc r10
-                inc r11
+                cmp r10, rax
+
                 dec rax
                 jnz .inner_loop_start
 
     .outer_loop_end:
-        sub r11, rcx                    ; Reset dst ptr X
-        lea r11, [r11 + GAME_BOARD_WIDTH]
+        sub r9, rcx                    ; Reset dst ptr X
+        lea r9, [r9 + GAME_BOARD_WIDTH]
+        inc r11
         dec rdx
         jnz .outer_loop_start
 
@@ -417,4 +561,3 @@ _can_move_piece:
     
     .return:
         ret
-
